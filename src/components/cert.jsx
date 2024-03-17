4c4219af6c0d6db94652de96c87de49e6272951e
charle426 <114236505+charle426@users.noreply.github.com>
2024-03-17 18:35:15 +0100

    build

--- src/components/cert.jsx
+++ src/components/cert.jsx
@@ -18,49 +18,43 @@ export default function Cert() {
             <div>
               <img
                 src={frontendCert}
-                className="w-full rounded-md"
+                className="w-full rounded-md max-w-[450px]"
               />
             </div>
             <div>
               <p>
-                Certificate of achievement for successful completion of Frontend Website Development Training in Webdeves
+                Certificate of achievement for successful completion of Frontend
+                Website Development Training in Webdeves
               </p>
-              <p className="text-[13px] my-2">
-                24th November 2023
-              </p>
+              <p className="text-[13px] my-2">24th November 2023</p>
             </div>
           </div>
           <div className="flex flex-col gap-3">
             <div>
               <img
                 src={fullstack}
-                className="w-full rounded-md"
+                className="w-full rounded-md max-w-[450px]"
               />
             </div>
             <div>
               <p>
-                This certificate is awarded to Uzoma Akachi Charles upon completion of Innovation Growth Hub Developers and Netpreneurs Fellowship Fullstack Website Development Training
+                This certificate is awarded to Uzoma Akachi Charles upon
+                completion of Innovation Growth Hub Developers and Netpreneurs
+                Fellowship Fullstack Website Development Training
               </p>
-              <p className="text-[13px] my-2">
-                19th December 2023
-              </p>
+              <p className="text-[13px] my-2">19th December 2023</p>
             </div>
           </div>
           <div className="flex flex-col gap-3">
             <div>
-              <img
-                src={codathon}
-                className="w-full rounded-md"
-              />
+              <img src={codathon} className="w-full rounded-md max-w-[450px]" />
             </div>
             <div>
               <p>
-                            For participating in Codathon 2023 <br />
-                            The Webdeves 3hrs long coding competition
-                        </p>
-              <p className="text-[13px] my-2">
-               Held on 21th November 2023
+                For participating in Codathon 2023 <br />
+                The Webdeves 3hrs long coding competition
               </p>
+              <p className="text-[13px] my-2">Held on 21th November 2023</p>
             </div>
           </div>
         </div>
